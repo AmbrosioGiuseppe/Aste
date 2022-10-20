ccbabbdb953acc3674ba96e0fc096cca7b580521
AmbrosioGiuseppe <grecommerce01@gmail.com>
2022-10-20 22:06:57 +0200

    First Commit

---
+++ Astematte/Astematte/class.py
@@ -0,0 +1,10 @@
+"""
+Classi generali da settare durante la creazione della Web App
+Inserire qui cosa c'è specificato in ogni classe in generale:
+- Utente            --> All'interno di questa classe saranno inseriti tutti i dati generali dell'utente
+- Impostazioni      --> All'interno di questa classe saranno inseriti tutti i dati generali dell'applicazione
+
+## Data aggiornamento 17/10/2022 ##
+## Autore: Giuseppe Ambrosio ##
+
+"""
---
+++ Astematte/Astematte/function.py
@@ -0,0 +1,8 @@
+"""
+Funzioni generali da settare durante la creazione della Web App
+Inserire qui cosa c'è specificato in ogni funzione in generale:
+
+## Data aggiornamento 17/10/2022 ##
+## Autore: Giuseppe Ambrosio ##
+
+"""
